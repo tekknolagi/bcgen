efb11f614dff67b5aebbfcb4ade8914ef5708bf9
Max Bernstein <max@bernsteinbear.com>
2024-10-06 11:07:06 -0400

    Add register enum for easier programming

--- example/main.c
+++ example/main.c
@@ -6,14 +6,14 @@ int main()
 	struct compile_state cs;
 	init(&cs);
 
-	select_movi(&cs, 2, 0); // i
-	select_movi(&cs, 1, 1000000000); // limit
-	select_movi(&cs, 0, 0); // total
+	select_movi(&cs, R2, 0); // i
+	select_movi(&cs, R1, 1000000000); // limit
+	select_movi(&cs, R0, 0); // total
 
 	bcval_t l = label(&cs); // top
-	select_addr(&cs, 0, 0, 2); // add iter to total
-	select_addi(&cs, 2, 2, 1);
-	bcreloc_t r = select_bltr(&cs, 2, 1, 0); // 0 is placeholder value,
+	select_addr(&cs, R0, R0, R2); // add iter to total
+	select_addi(&cs, R2, R2, 1);
+	bcreloc_t r = select_bltr(&cs, R2, R1, 0); // 0 is placeholder value,
 						// should maybe add in an
 						// UNDEFINED macro or something
 	end(&cs);
